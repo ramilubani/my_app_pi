6872677d43ef22c9ec925da2523e6d064e1294d8
ramilubani <ramilubani@yahoo.com>
2024-11-11 10:56:00 +0100

    11111055

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myapp</title>
+</head>
+<body>
+    <div style="border:3px solid black;">
+        <h2>Register</h2>
+        <form action="/register" method="post">
+            <input type="text" name="name" placelolder="name">
+            <input type="text" name="email" placelolder="email">
+            <input type="password" name="password" placelolder="password">
+            <button type="button">Register</button>
+            </form>
+    </div>
+</body>
+</html>
