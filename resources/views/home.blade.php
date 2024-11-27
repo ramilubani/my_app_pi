4a9d1fe39b89bc384726d05537636cc6e621097b
ramilubani <ramilubani@yahoo.com>
2024-11-27 08:56:59 +0000

    from debian

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,36 @@
     <title>myapp</title>
 </head>
 <body>
+<<<<<<< HEAD
     <div style="border:3px solid black;">
+=======
+    <div class="container">
+        @auth
+        <p class="message">Congrats, you are logged in!</p>
+        <form action="/logout" method="post">
+            @csrf
+            <div class="form-group">
+                <button>Logout</button>
+            </div>
+        </form>
+        <h2>Create a New Post</h2>
+        <form action="/create-post" method="post">
+            @csrf
+            <div class="form-group">
+                <input type="text" name="title" placeholder="Post title">
+            </div>
+            <div class="form-group">
+                <textarea name="body" placeholder="Body content..."></textarea>
+            </div>
+            <div class="form-group">
+                <button>Save Post</button>
+            </div>
+            <input type="text" name="Title" placeholder="post title">
+            <textarea name="body" placeholder="body content ..."></textarea>
+            <button>Save Post</button>
+        </form>
+        @else
+>>>>>>> 7448693 (commit from debian)
         <h2>Register</h2>
         <form action="/register" method="post">
             @csrf
